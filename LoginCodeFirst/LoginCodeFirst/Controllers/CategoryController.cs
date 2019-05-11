72d4cbc01d160cf6e7eeeafe68edd5086891b7c8
Hoang Van Huan <huanhoang2121@gmail.com>
2019-05-11 20:49:32 +0700

    Update Project

--- LoginCodeFirst/LoginCodeFirst/Controllers/CategoryController.cs
+++ LoginCodeFirst/LoginCodeFirst/Controllers/CategoryController.cs
@@ -5,47 +5,49 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LoginCodeFirst.Controllers
 {
-        public class CategoryController : Controller
-        {
-            private readonly ICategoryServices _categoryServices;
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryServices _categoryServices;
 
-            public CategoryController(ICategoryServices categoryServices)
-            {
-                _categoryServices  = categoryServices;
-            }
-            
-     
+        public CategoryController(ICategoryServices categoryServices)
+        {
+            _categoryServices = categoryServices;
+        }
+
+
         [HttpGet]
         public async Task<IActionResult> Index()
         {
             var liststore = await _categoryServices.GetListAsync();
             return View(liststore);
-        } 
-            
-   
+        }
+
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
         }
-       
-        
+
+
         [HttpPost]
         public async Task<IActionResult> Add(IndexViewModel category)
         {
 
             if (ModelState.IsValid)
-            { 
+            {
                 var list = await _categoryServices.Add(category);
-                if (list != null)
+                if (list)
                 {
                     TempData["Category"] = "Add Category Success";
                     return RedirectToAction("Index");
                 }
+                ViewBag.ErrorMessage = "Add Category Failure";
+                return View(category);
             }
             ViewBag.ErrorMessage = "Add Category Failure";
             return View(category);
-        }   
+        }
         [HttpGet]
         public async Task<IActionResult> Edit(int? id)
         {
@@ -61,29 +63,29 @@ namespace LoginCodeFirst.Controllers
 
             return View(list);
         }
-        
+
         [HttpPost]
-        public async Task<IActionResult> Edit(int? id,IndexViewModel category)
+        public async Task<IActionResult> Edit(int? id, IndexViewModel category)
         {
 
             if (ModelState.IsValid)
             {
-                  await _categoryServices.Edit(category);
-                    TempData["Category"] = "Edit Category Success";
-                  return RedirectToAction("Index");
-               
+                await _categoryServices.Edit(category);
+                TempData["Category"] = "Edit Category Success";
+                return RedirectToAction("Index");
+
             }
 
             ViewBag.Err = "Edit Category Failure";
             return View(category);
         }
-        
+
         [HttpGet]
         public async Task<IActionResult> Delete(int? id)
         {
             await _categoryServices.Delete(id);
             TempData["Category"] = "Delete Category Success";
-            return  RedirectToAction("Index");
+            return RedirectToAction("Index");
         }
-    } 
+    }
 }
